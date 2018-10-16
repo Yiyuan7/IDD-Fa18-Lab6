665ff8fb9737f03f931fa2b9639c8603f95eb0a8
yiyuan7 <cherfyy@gmail.com>
2018-10-15 22:41:16 -0700

    homework 6666

--- chatServer.js
+++ chatServer.js
@@ -1,7 +1,9 @@
 /*
 chatServer.js
 Author: David Goedicke (da.goedicke@gmail.com)
-Closley based on work from Nikolas Martelaro (nmartelaro@gmail.com) as well as Captain Anonymous (https://codepen.io/anon/pen/PEVYXz) who forked of an original work by Ian Tairea (https://codepen.io/mrtairea/pen/yJapwv)
+Closley based on work from Nikolas Martelaro (nmartelaro@gmail.com)
+as well as Captain Anonymous (https://codepen.io/anon/pen/PEVYXz)
+who forked of an original work by Ian Tairea (https://codepen.io/mrtairea/pen/yJapwv)
 */
 
 var express = require('express'); // web server application
@@ -30,8 +32,8 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function() { // we wait until the client has loaded and contacted us that it is ready to go.
 
-    socket.emit('answer', "Hey, hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-    setTimeout(timedQuestion, 5000, socket, "What is your name?"); // Wait a moment and respond with a question.
+    socket.emit('answer', "Hello! I'm FYY"); //We start with the introduction;
+    setTimeout(timedQuestion, 2000, socket, "What is your name?"); // Wait a moment and respond with a question.
 
   });
   socket.on('message', (data) => { // If we get a new message from the client we process it;
@@ -51,51 +53,61 @@ function bot(data, socket, questionNum) {
 
   /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-    answer = 'Hello ' + input + ' :-)'; // output response
-    waitTime = 5000;
-    question = 'How old are you?'; // load next question
+    answer = 'Hey it\'s so good to see yo here! '; // output response
+    waitTime = 2000;
+    question = 'OK, ' + input + ', I think you have a crash on someone recently ❤❤❤❤ Is that right??'; // load next question
   } else if (questionNum == 1) {
-    answer = 'Really, ' + input + ' years old? So that means you were born in: ' + (2018 - parseInt(input)); // output response
-    waitTime = 5000;
-    question = 'Where do you live?'; // load next question
-  } else if (questionNum == 2) {
-    answer = 'Cool! I have never been to ' + input + '.';
-    waitTime = 5000;
-    question = 'Whats your favorite color?'; // load next question
-  } else if (questionNum == 3) {
-    answer = 'Ok, ' + input + ' it is.';
-    socket.emit('changeBG', input.toLowerCase());
-    waitTime = 5000;
-    question = 'Can you still read the font?'; // load next question
-  } else if (questionNum == 4) {
-    if (input.toLowerCase() === 'yes' || input === 1) {
-      answer = 'Perfect!';
-      waitTime = 5000;
-      question = 'Whats your favorite place?';
-    } else if (input.toLowerCase() === 'no' || input === 0) {
-      socket.emit('changeFont', 'white'); /// we really should look up the inverse of what we said befor.
-      answer = ''
-      question = 'How about now?';
-      waitTime = 0;
-      questionNum--; // Here we go back in the question number this can end up in a loop
-    } else {
-      question = 'Can you still read the font?'; // load next question
-      answer = 'I did not understand you. Could you please answer "yes" or "no"?'
-      questionNum--;
-      waitTime = 5000;
-    }
-    // load next question
-  } else {
-    answer = 'I have nothing more to say!'; // output response
+    if (input === 'no'){
+    answer = 'You are lying! You love me, don\'t you????????' ;
     waitTime = 0;
     question = '';
-  }
+    }
+    else{
+    answer = 'I know who she is. Trust me. ' ; // output response
+    waitTime = 2000;
+    question = 'Is she one of your friends?';
+    }// load next question
+  } else if (questionNum == 2) {
+    if (input==='yes'){
+    answer = 'I know I\'m right' ;
+    waitTime = 2000;
+    question = 'Have you hang out with her during this week??';
+    }
+    else{
+      answer = 'I think it will be a good idea to make friend with her.' ;
+      waitTime = 0;
+      question = '';
+    }// load next question
+  } else if (questionNum == 3) {
+    answer = 'emmmm. You know, I\'m nearly know who she is';
+    waitTime = 2000;
+    if (input==='yes'){
+    question = 'Her name is Shawn?';
+    }
+    else{
+    question = 'Come on! What are you waiting for! Go to ask her to watch warriors and nets game together!!!';
+    }
+  } else if (questionNum == 4) {
+    if (input === 'yes' || input === 'ok') {
+      answer = 'I\'m so proud of myself!! ❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤ ';
+      waitTime = 1000;
+      question ="";
+
+      }
+      else {
+      answer = 'I have nothing more to say!'; // output response
+      waitTime = 0;
+      question = '';
+      }/// we really should look up the inverse of what we said befor.
+
+    }
 
 
   /// We take the changed data and distribute it across the required objects.
   socket.emit('answer', answer);
   setTimeout(timedQuestion, waitTime, socket, question);
   return (questionNum + 1);
+
 }
 
 function timedQuestion(socket, question) {
